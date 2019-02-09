03841e66726e89d5c11e2acc321519358d764849
Subraiz Ahmed <subraizahmed@gmail.com>
2019-02-09 08:07:25 -0500

    Update Instagram.swift

--- HackBeanpot/Instagram.swift
+++ HackBeanpot/Instagram.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestore
+
+
+
+class Instagram {
+    let db = Firestore.firestore();
+    
+    func setQuery(query: String) -> () {
+        let formattedQuery = query.replacingOccurrences(of: " ", with: "")
+        print(formattedQuery)
+        db.collection("Queries").document("queryField").setData(["Name": formattedQuery])
+    }
+    
+    func fetchData() {
+        db.collection("Results").document("resultField").addSnapshotListener { documentSnapshot, error in
+            guard let document = documentSnapshot else {
+                print("Error fetching document: \(error!)")
+                return
+            }
+            guard let data = document.data() else {
+                print("Document data was empty.")
+                return
+            }
+            print("Current data: \(data)")
+        }
+    }
+}
