6c0a62ec6e77d6e072e83b329849a373c84c05fd
John Abreu <abreuja@bc.edu>
2019-02-08 22:41:49 -0500

    Created xcodeproj

---
+++ Pods/Target Support Files/Pods-HackBeanpot/Pods-HackBeanpot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HackBeanpot : NSObject
+@end
+@implementation PodsDummy_Pods_HackBeanpot
+@end
---
+++ Pods/Target Support Files/Pods-HackBeanpotUITests/Pods-HackBeanpotUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HackBeanpotUITests : NSObject
+@end
+@implementation PodsDummy_Pods_HackBeanpotUITests
+@end
